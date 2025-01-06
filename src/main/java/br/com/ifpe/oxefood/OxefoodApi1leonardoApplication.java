da0455827c8350f5a3c833f7c12ee1f7fd9f1812
Leonardo <lcl5@discente.ifpe.edu.br>
2025-01-06 14:39:05 -0300

    primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApi1leonardoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApi1leonardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApi1leonardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxefood/OxefoodApi1leonardoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodApi1leonardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
